82c2f963c7e360dd37c9aa5a89a562b0367fdb3a
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-01-24 13:51:08 +0530

    Update pushbullet.py

--- pushbullet.py
+++ pushbullet.py
@@ -4,26 +4,27 @@ from time import sleep
 
 cam = cv2.VideoCapture(0)
 image = 0
+api="o.kWWLMHhCVKfYb5SiWkcnc6nihHkKfRPC"
 
 def CaptureImage():
 	print("Capturing Image :")
 	ret, image = cam.read()
 	k = cv2.waitKey(1000)
 	print("Image Capture successfully")
-	cv2.imwrite('/home/pi/Desktop/testimage.jpg',image)
+	cv2.imwrite('testimage.jpg',image)
 	cam.release()
 	cv2.destroyAllWindows()
 	print("Sending the Notification")
 
 def SendNotification():
-	pb = Pushbullet(o.TP46NeW52dnB2LDTeY96tWsuDBORNwIr)
-	dev = pb.get_device("Xiaomi 22120RN86I")
-	with open('/home/pi/Desktop/testimage.jpg','rb') as pic:
+	pb = Pushbullet(api)
+	dev = pb.get_device("Xiaomi 22120RN86I")  ##devic name
+	with open('testimage.jpg','rb') as pic:
 		file_data = pb.upload_file(pic,"Picture.jpg")
 		push = pb.push_file(**file_data)
 		Value = push["active"]
 		if Value == True:
-			print("Messagesent successfully...")
+			print("Message sent successfully...")
 		else:
 			print("Sending Message Failed..")
 

50f2e94f43105838a7f80ad1e1d75c310fbfb288
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-01-26 20:48:14 +0530

    Update servo.py

--- servo.py
+++ servo.py
@@ -4,7 +4,6 @@ from gpiozero import AngularServo
 
 
 root = Tk()
-root.geometry('600x150')
 
 def rotate():
 	servo = AngularServo(17, min_angle = -90, max_angle = 90)
@@ -14,13 +13,14 @@ def rotate():
 		sleep(20)
 
 V1 = DoubleVar()
-TitleLabel = Label(root,text = "select the angle of rotation using slider")
-TitleLabel.config(font = ('helvetica',15))
-TitleLabel.config(fg = "#DE3163")
+Title = Label(root,text = "select the angle of rotation using slider")
+
 
 Slider = Scale(root, variable = V1,from_ = -90, to=90, length=720, tickinterval=15, orient = "horizontal")
-SubmitBtn = Button(root,text = "Click here to rotate ",command = lambda: rotate(), bg= 'Yellow')
-TitleLabel.pack(anchor = CENTER)
+Submit = Button(root,text = "Click here to rotate ",command = lambda: rotate()) #, bg= 'Yellow')
+
+Title.pack(anchor = CENTER)
 Slider.pack(anchor = CENTER)
-SubmitBtn.pack(anchor = CENTER)
+Submit.pack(anchor = CENTER)
+
 root.mainloop()

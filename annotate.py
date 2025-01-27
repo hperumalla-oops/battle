a75b17cad58907f55de2b3de098644362c2a6efe
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-01-27 13:02:21 +0530

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -4,28 +4,25 @@ cam = cv2.VideoCapture(0)
 image = 0
 
 
+
 def CaptureImage():
-	global image
 	print("Capturing Image :")
 	ret, image = cam.read()
-	k = cv2.waitKey(1000)
 	print("Image Capture successfully")
-	cv2.imwrite('testimage.jpg',image)
-	cam.release()
 	cv2.destroyAllWindows()
+	return image
+	
 
-def AnnotateImage():
+def AnnotateImage(image):
 	print("print Annotation Completed..")
-	ImageCopy = cv2.imread('testimage.jpg',image)
-	print("Dimension of the Image :",ImageCopy.shape)
+	print("Dimension of the Image :",image.shape)
 	Start = (0,340)
 	End = (100,480)
-	cv2.rectangle(ImageCopy , Start,End,(0,0,255))
-	cv2.putText(ImageCopy, "pen_stand", (0,330), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0))
+	cv2.rectangle(image , Start,End,(0,0,255))
+	cv2.putText(image, "pen_stand", (0,330), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0))
 	
-	cv2.imshow('AnnotatedImage', ImageCopy)
+	cv2.imshow('AnnotatedImage', image)
 	cv2.waitKey(10000)
 
 
-CaptureImage()
-AnnotateImage()
+AnnotateImage(CaptureImage())

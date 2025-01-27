ec6b808f6e8ffb8e92ccd54f6f5c177e8f1d79f8
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-01-27 12:55:27 +0530

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -17,15 +17,15 @@ def CaptureImage():
 def AnnotateImage():
 	print("print Annotation Completed..")
 	ImageCopy = cv2.imread('testimage.jpg',image)
-	ImageDimension = ImageCopy.shape
-	print("Dimension of the Image :",str(ImageDimension))
+	print("Dimension of the Image :",ImageCopy.shape)
 	Start = (0,340)
 	End = (100,480)
-	cv2.rectangle(ImageCopy , Start,End,(0,0,255),thickness = 3, lineType = cv2.LINE_8)
-	cv2.putText(ImageCopy, "pen_stand", (0,330), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),1)
+	cv2.rectangle(ImageCopy , Start,End,(0,0,255))
+	cv2.putText(ImageCopy, "pen_stand", (0,330), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0))
 	
 	cv2.imshow('AnnotatedImage', ImageCopy)
 	cv2.waitKey(10000)
+
 
 CaptureImage()
 AnnotateImage()
